e7cf62f439a0aac746d96ae51e617f1ff0bb75ab
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-03 11:30:45 +0530

    added the route to add friend not done yet

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,7 +1,15 @@
+import { addFriendValidator } from "@/lib/validations/add-friend"
+
 export async function POST(req:Request){
     try {
-        
+        const body = await req.json()
+        const{email:emailToAdd} = addFriendValidator.parse(body.email)
+
+        const RESTResponse = await
+
     } catch (error) {
         
+        
+       console.log(err) 
     }
 }
